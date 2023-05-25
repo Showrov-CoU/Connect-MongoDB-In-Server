60b9d348eff3ab46bf1be137df0a43186a814372
Md.Ashikur Rahman (Showrov) <showrov.cse.cou@gmail.com>
2023-05-26 03:04:12 +0600

    Practice delete of CRUD operation

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//! ConnectDB
 const connectionDB = async () => {
   try {
     await mongoose.connect("mongodb://127.0.0.1:/testDB");
@@ -18,6 +19,7 @@ const connectionDB = async () => {
   }
 };
 
+//! Create Schema
 const productDetails = new mongoose.Schema({
   Title: {
     type: String,
@@ -36,61 +38,62 @@ const productDetails = new mongoose.Schema({
     default: Date.now,
   },
 });
+
 //! Model
 const Product = mongoose.model("allProducts", productDetails);
 
 //! CREATE Product
 app.post("/productRoute", async (req, res) => {
   try {
-    // const createProduct = new Product({
-    //   Title: req.body.title,
-    //   Price: req.body.price,
-    //   Description: req.body.description,
-    // });
-    // const productData = await createProduct.save();
-    const productData = await Product.insertMany([
-      {
-        Title: "iphone 5",
-        Price: 5000,
-        Description: "iphone 5 is a Nice Phone",
-      },
-      {
-        Title: "iphone 6",
-        Price: 6000,
-        Description: "iphone 6 is a Nice Phone",
-      },
-      {
-        Title: "iphone 7",
-        Price: 7000,
-        Description: "iphone 7 is a Nice Phone",
-      },
-    ]);
+    const createProduct = new Product({
+      Title: req.body.title,
+      Price: req.body.price,
+      Description: req.body.description,
+    });
+    const productData = await createProduct.save();
+
     res.status(201).send({
       productData,
     });
   } catch (error) {
     res.status(500).send({
-      msg: "Something is wrong",
+      success: false,
       message: error.message,
     });
   }
 });
 
-//! READ Product
+//! Read all Product
+app.get("/productRoute", async (req, res) => {
+  try {
+    const productData = await Product.find();
+    res.status(200).send({
+      success: true,
+      data: productData,
+      message: "These are the Products",
+    });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+//! READ Specific Product
 app.get("/productRoute/:id", async (req, res) => {
   try {
     const id = req.params.id;
-    // const productData = await Product.findOne({ _id: id }).select({
-    //   Title: 1,
-    //   Description: 1,
-    //   _id: 0,
-    // });
     const productData = await Product.findOne(
       { _id: id },
       { Title: 1, Price: 1, _id: 0 }
     );
     if (productData) {
-      res.status(200).send({ productData });
+      res.status(200).send({
+        success: true,
+        data: productData,
+        message: "Find Sepcific Product by product's id",
+      });
     } else {
       res.status(404).send({
         message: "Product Not Found",
@@ -98,10 +101,36 @@ app.get("/productRoute/:id", async (req, res) => {
     }
   } catch (error) {
     res.status(500).send({
+      success: false,
       message: error.message,
     });
   }
 });
+
+//! Delete Product.
+app.delete("/productRoute/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const productData = await Product.deleteOne({ _id: id });
+    if (productData) {
+      res.status(200).send({
+        success: true,
+        data: productData,
+        message: "This Product is deleted",
+      });
+    } else {
+      res.status(404).send({
+        message: "Product Not Found",
+      });
+    }
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
 //! Server Section
 const port = 3000;
 app.listen(port, async () => {
